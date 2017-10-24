2445bd42d885ee486c0dbd4b98753cd9159c204a
Olusola Oseni <kenolusola@gmail.com>
2017-10-24 11:54:28 +0100

    Add test folder, app.js and install babel

---
+++ tests/test.js
@@ -0,0 +1,18 @@
+import chai from 'chai'
+const expect = chai.expect
+
+describe('', () => {
+    describe('', () => {
+
+      it('', () => {
+        expect().to.be.equal()
+      });
+      it('', () => {
+        expect().to.be.equal()
+      });
+      it('', () => {
+        expect().to.be.equal()
+      });
+      
+    });
+})
